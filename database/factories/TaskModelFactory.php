9d85df29bb0d80537405e8841987eb3fc3ca821d
amit4591 <amit@zestasoft.com>
2017-04-20 13:47:41 +0530

    database seeder implemented for task

--- database/factories/TaskModelFactory.php
+++ database/factories/TaskModelFactory.php
@@ -11,15 +11,12 @@
 |
 */
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+    //static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'title' => $faker->name,
+        'description' => $faker->paragraph
     ];
 });
- */
